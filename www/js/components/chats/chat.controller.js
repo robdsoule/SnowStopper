0affda715cb459002396940fd8ceb24d04f8cbe2
rsoule3 <robsoule3@gmail.com>
2016-01-10 18:36:54 -0600

    Converted All ng-controller directives to use As syntax
    I find this syntax easier to read, ie. AccountCtrl as account is less ambiguous
    than seemingly random variable names on $scope.  Replaced $scope in controllers
    with 'this' keyword.

--- www/js/components/chats/chat.controller.js
+++ www/js/components/chats/chat.controller.js
@@ -2,7 +2,7 @@
   'use strict';
 
   function ChatController($scope, $stateParams, ChatsService) {
-    $scope.chat = ChatsService.get($stateParams.chatId);
+    this.chatter = ChatsService.get($stateParams.chatId);
   }
 
   module.exports = ['$scope', '$stateParams', 'ChatsService', ChatController];
